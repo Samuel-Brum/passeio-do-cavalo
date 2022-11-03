be75d5b596910e7541871919c71fffc83d8ff512
Samuel-Brum <samuelbrummartins@gmail.com>
2022-11-03 15:24:31 -0300

    corrige a implementação, apesar de ser ingênua e pouco eficiente

--- passeio.h
+++ passeio.h
@@ -2,11 +2,12 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define index(i,j) i*N + j
 #define FALSE -1
 #define TRUE 1
-#define N 5
+#define N 7
 
 typedef struct {
     int linha;
@@ -30,6 +31,7 @@ void imprime_tabuleiro(int* tabuleiro) {
     printf("\n");
     }
     printf("\n");
+    //sleep(1);
 }
 
 void movimento_invalido (coordenada* movimento) {
@@ -37,16 +39,16 @@ void movimento_invalido (coordenada* movimento) {
     movimento->coluna = FALSE;
 }
 
-int passo (int* tabuleiro, int turno, int posicao, coordenada* movimento_anterior) {
+int passo (int* tabuleiro, int turno, int posicao, coordenada* movimento_anterior, int* casas_retrocedidas, int* casas_avancadas) {
     int linha = posicao / N;
     int coluna = posicao % N;
-
+    //imprime_tabuleiro(tabuleiro);
 
     coordenada movimentos[8];
     int count = 0;
 
-    for (int i = -2; i <= 2; i++) {
-        for (int j = -2; j <= 2; j++) {
+    for (int i = 2; i >= -2; i--) {
+        for (int j = 2; j >= -2; j--) {
             if (i == j || i == 0 || j == 0 || abs(i) == abs(j)) {
                 continue;
             }
@@ -74,32 +76,40 @@ int passo (int* tabuleiro, int turno, int posicao, coordenada* movimento_anterio
         int casa = index(movimentos[i].linha, movimentos[i].coluna);
         if (movimentos[i].linha != FALSE) {
             tabuleiro[casa] = turno + 1;
-            //imprime_tabuleiro(tabuleiro);
-            if (passo(tabuleiro, turno + 1, casa, &movimentos[i])) {
-                imprime_tabuleiro(tabuleiro);
+            *casas_avancadas += 1;
+            if (turno == N * N - 1) {
                 return TRUE;
             }
-        } else { // !!! BUGADO AQUI, CONSERTAR !!!
-        
-                }
+            if (passo(tabuleiro, turno + 1, casa, &movimentos[i], casas_retrocedidas, casas_avancadas)) {
+                return TRUE;
             }
-        }
-    }
+            tabuleiro[casa] = 0;
+            *casas_retrocedidas += 1;
+        } if (i == 7) {  
+            turno -= 1;
+            return 0;
+            }
+        }        
 }
+    
 
 
 void passeio (int x, int y) {
-    x = x-1; // normaliza a notação para adequar ao formato 
+    x = x-1; // normaliza a notação para adequar ao formato requerido
     y = y-1; // do resto do código (1, 1) -> (0, 0) || (3, 2) -> (2, 1) ...
 
     int posicao_inicial = index(x, y);
 
+    // ajusta parametros da função 
     int tabuleiro[N * N];
     cria_tabuleiro(tabuleiro);
     int turno = 1;
+    int casas_retrocedidas = 0;
+    int casas_avancadas = 1;
     tabuleiro[posicao_inicial] = turno;
 
-    imprime_tabuleiro(tabuleiro);
+    passo(tabuleiro, turno, posicao_inicial, NULL, &casas_retrocedidas, &casas_avancadas);
 
-    passo(tabuleiro, turno, posicao_inicial, NULL);
-}
+    imprime_tabuleiro(tabuleiro);
+    printf("%d %d", turno, casas_retrocedidas);
+} // serve de casca para a chamada especificada
